1281812b167027fcbb24301220728752b2ce9dcd
Subhrajit <er.subhrajitm@gmail.com>
2026-01-29 15:26:17 +0530

    tauri-u6

--- src-tauri/target/debug/deps/toml_datetime-fb2e8e82fc32d04c.d
+++
@@ -1,8 +0,0 @@
-/Users/subhrajitmandal/Documents/NewProjects/envello-app/src-tauri/target/debug/deps/toml_datetime-fb2e8e82fc32d04c.d: /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/lib.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/datetime.rs
-
-/Users/subhrajitmandal/Documents/NewProjects/envello-app/src-tauri/target/debug/deps/libtoml_datetime-fb2e8e82fc32d04c.rlib: /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/lib.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/datetime.rs
-
-/Users/subhrajitmandal/Documents/NewProjects/envello-app/src-tauri/target/debug/deps/libtoml_datetime-fb2e8e82fc32d04c.rmeta: /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/lib.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/datetime.rs
-
-/Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/lib.rs:
-/Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/toml_datetime-0.6.11/src/datetime.rs:
--- src-tauri/target/debug/deps/utf8-e8f567a2b57d25c4.d
+++
@@ -1,9 +0,0 @@
-/Users/subhrajitmandal/Documents/NewProjects/envello-app/src-tauri/target/debug/deps/utf8-e8f567a2b57d25c4.d: /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lib.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lossy.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/read.rs
-
-/Users/subhrajitmandal/Documents/NewProjects/envello-app/src-tauri/target/debug/deps/libutf8-e8f567a2b57d25c4.rlib: /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lib.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lossy.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/read.rs
-
-/Users/subhrajitmandal/Documents/NewProjects/envello-app/src-tauri/target/debug/deps/libutf8-e8f567a2b57d25c4.rmeta: /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lib.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lossy.rs /Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/read.rs
-
-/Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lib.rs:
-/Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/lossy.rs:
-/Users/subhrajitmandal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utf-8-0.7.6/src/read.rs:
